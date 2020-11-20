5fffb4709380705cadb7a200afe047056d0e1e84
Olaf Timme <octimme@gmail.com>
2020-11-20 13:46:01 +0100

    pod 0.0.9-alpha

--- BBNativePlayerKit.framework/Headers/BBNativePlayerKit.h
+++ BBNativePlayerKit.framework/Headers/BBNativePlayerKit.h
@@ -15,3 +15,5 @@ FOUNDATION_EXPORT double BBNativePlayerKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char BBNativePlayerKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BBNativePlayerKit/PublicHeader.h>
+
+
