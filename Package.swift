155c90c227826d8a9cf77e0789e45e72b6ad13a0
Tiago Queirós <tiagosp.queiros@gmail.com>
2024-06-03 14:54:58 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/bluebillywig/bbnativeshared-cocoapod", exact: "8.1.0"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios", exact: "3.19.1"),
-        .package(url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform", "1.1.0"..<"3.0.0")
+        .package(name: "GoogleUserMessagingPlatform", url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform.git", "1.1.0"..<"3.0.0")
     ],
     targets: [
         .binaryTarget(
